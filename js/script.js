de153cbce2fedda714682d23f37abc4b161c7bba
raphatapi <raphaeltapioca10@gmail.com>
2020-05-02 09:08:52 -0600

    add scrolling, fix content

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
+    $(".nav-item").on('click', function(event) {
   
       // Make sure this.hash has a value before overriding default behavior
       if (this.hash !== "") {
@@ -13,7 +13,7 @@ $(document).ready(function(){
         // Using jQuery's animate() method to add smooth page scroll
         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
         $('html, body').animate({
-          scrollTop: $(hash).offset().top
+          scrollTop: $(hash).offset().top - $("nav").outerHeight(true)
         }, 800, function(){
   
           // Add hash (#) to URL when done scrolling (default click behavior)
