1aaf66538fcc531c3d03e3f4d7bbf34d23199730
raphatapi <raphaeltapioca10@gmail.com>
2020-05-13 21:27:34 -0600

    sections

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     // Add smooth scrolling to all links
-    $(".nav-item").on('click', function(event) {
+    $(".nav-link").on('click', function(event) {
   
       // Make sure this.hash has a value before overriding default behavior
       if (this.hash !== "") {
