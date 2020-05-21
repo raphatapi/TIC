c4a56d065f56fdde7dcd7f4e8948075c127f88ba
raphatapi <raphaeltapioca10@gmail.com>
2020-05-20 21:18:56 -0600

    stick navbar

--- js/script.js
+++ js/script.js
@@ -1,33 +1,26 @@
 $(document).ready(function(){
-    // // Add smooth scrolling to all links
-    // $(".nav-link").on('click', function(event) {
-    //   // Prevent default anchor click behavior
-    //   event.preventDefault();
-  
-    //   // Store hash
-    //   var hash = this.hash;
-    //   // Make sure this.hash has a value before overriding default behavior
-    //   if (this.hash !== "") {
-        
-  
-    //     // Using jQuery's animate() method to add smooth page scroll
-    //     // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-    //     $('html, body').animate({
-    //       scrollTop: $(hash).offset().top - $("nav").outerHeight(true)
-    //     }, 800, function(){
-  
-    //       // Add hash (#) to URL when done scrolling (default click behavior)
-    //       window.location.hash = hash;
-    //     });
-    //   } // End if
-    // });
-
-
+    
     $('.nav-link').click(function(){    
       var divId = $(this).attr('href');
        $('html, body').animate({
         scrollTop: $(divId).offset().top - $("nav").outerHeight(true)
       }, 100);
+    });
+
+    $(".ct-btn-scroll").click(function(event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+          // Store hash
+          var hashTop = this.hash;
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (1200) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+              scrollTop: $(hashTop).offset().top - $("nav").outerHeight(true) + 1
+            }, 100, function(){
+          });
+      } // End if
     });
 
     
